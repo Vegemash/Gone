68c67867fb7c95d694edee9df87c3c6d68c3fc03
Vegemash <mrisaacjackson@gmail.com>
2016-09-19 09:35:09 +1000

    Program now prints the first 100 primes with their first digits being (1-100)

--- primer/primer.go
+++ primer/primer.go
@@ -1,38 +1,48 @@
-//Prime finds the first 1000000 primes and prints them
-package primer
+package main
 
 import "fmt"
-
+import "math"
 
 func main() {
-	fmt.Println(FirstNPrimes(1000000));
+	primes := FirstNPrimes(10000)
+	var countingPrimes []int
+	countingPrimesCounter := 1
+	counter := 0
+	for countingPrimesCounter <= 100 && counter < len(primes) {
+		if intNSigFigs(primes[counter], intLog10(countingPrimesCounter)+1) == countingPrimesCounter {
+			countingPrimes = append(countingPrimes, primes[counter])
+			countingPrimesCounter++
+		}
+		counter++
+	}
+	fmt.Println(countingPrimes)
+	fmt.Println(primes[len(primes)-1])
 }
 
-//Returns the first 'goal' primes
-//
-func FirstNPrimes(goal int) []int{
-	primes := make([]int, 0)
+//FirstNPrimes : returns the first 'goal' primes
+func FirstNPrimes(goal int) []int {
+	var primes []int
 
 	//Defencive guards
-	if goal == 0{
+	if goal == 0 {
 		return []int(primes)
 	}
 
-	primes = append (primes, 2)
+	primes = append(primes, 2)
 
 	counter := 3
 	//Loop the counter until we have the goal amount of primes
-	for len(primes) < goal	{
+	for len(primes) < goal {
 		primesCounter := 0
 		prime := true
-		//while the prime we are checking the counter against is 
+		//while the prime we are checking the counter against is
 		//less than the square root of the counter
-		for counter > (primes[primesCounter] * primes[primesCounter]) {
+		for counter >= (primes[primesCounter] * primes[primesCounter]) {
 			//if the prime cleanly divides the counter the counter is not prime
-			if(counter % primes[primesCounter] == 0){
+			if counter%primes[primesCounter] == 0 {
 				prime = false
 				break
-			}else{
+			} else {
 				//check against the n ext prime
 				primesCounter++
 			}
@@ -48,4 +58,21 @@ func FirstNPrimes(goal int) []int{
 		counter += 2
 	}
 	return []int(primes)
+}
+
+func intNSigFigs(in int, n int) int {
+	digits := intLog10(in) + 1
+	if digits <= n {
+		return in
+	}
+
+	return in / intPow(10, digits-n)
+}
+
+func intLog10(in int) int {
+	return int(math.Log10(float64(in)))
+}
+
+func intPow(in int, pow int) int {
+	return int(math.Pow(float64(in), float64(pow)))
 }
